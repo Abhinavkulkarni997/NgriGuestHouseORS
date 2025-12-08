9bfc1ece9110b06e5598d793d0901621d4f838d6
unknown <Abhinavkulkarni997>
2025-12-08 16:16:38 +0530

    new features and files added

--- Frontend/src/components/GuestHouseGallery/GuestHouseGallery.jsx
+++ Frontend/src/components/GuestHouseGallery/GuestHouseGallery.jsx
@@ -77,8 +77,10 @@ import thg6 from '../../assets/Gallery/thg6.jpg';
 import thg7 from '../../assets/Gallery/thg7.jpg';
 import { FaCircleChevronRight } from "react-icons/fa6";
 import { FaCircleChevronLeft } from "react-icons/fa6";
-
-
+import {Swiper,SwiperSlide} from "swiper/react";
+import {Navigation,Autoplay} from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation"
 const GuestHouseGallery = () => {
     const gallery=[{
         id:1,
@@ -100,37 +102,35 @@ const GuestHouseGallery = () => {
         id:6,
         img:thg7
     }]
-
-    const [galleryNext,setGalleryNext]=useState();
-    const toggleRight=()=>{
-        setGalleryNext(galleryNext=>galleryNext+1);
-    }
-
-    const  toggleLeft=()=>{
-        setGalleryNext(galleryNext=>galleryNext-1);
-    }
-
   return (
     <section className='w-full py-16 sm:py-14 lg:py-20  '>
         <div className='max-w-6xl mx-auto bg- px-4 '>
             <h1 className='text-3xl md:text-4xl font-extrabold text-gray-800 mb-8 text-center'>Gallery</h1>
-            <Swiper>
+            <Swiper modules={[Navigation,Autoplay]}
+            navigation autoplay={{delay:2200}}
+            spaceBetween={20}
+            loop={true}
+ breakpoints={{
+            320: { slidesPerView: 1.2 },
+            480: { slidesPerView: 1.5 },
+            640: { slidesPerView: 2 },
+            768: { slidesPerView: 2.5 },
+            1024: { slidesPerView: 3 },
+            1280: { slidesPerView: 4 },
+          }}            className='pb-10'>
+
             {gallery.map((galleryItem, index) => (
-                <div key={index}> 
-                <div className='relative overflow-hidden rounded-lg shadow-lg group'>
+                <SwiperSlide key={index}>
+            
+                <div className=' overflow-hidden rounded-lg shadow-lg group cursor-pointer'>
                 {<img src={galleryItem.img} 
                 className='w-full h-48 md:h-56 object-cover transition-transform duration-300 group-hover:scale-110' alt='GuestHouse'/>}
                 </div>
-
-                </div>
+                </SwiperSlide>
             ))}
-             </Swiper>
+            </Swiper>
+            
             </div>
-           
-            <div className='flex justify-center items-center gap-4'>
-            <FaCircleChevronLeft  onClick={toggleLeft} size={24} className='bg-white rounded-full text-indigo-500'/>
-            <FaCircleChevronRight onClick={toggleRight} size={24} className='bg-white rounded-full text-indigo-500'/>
-        </div>
     </section>
   )
 }
