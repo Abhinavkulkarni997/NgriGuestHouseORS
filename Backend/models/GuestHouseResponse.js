5101be6426bab8277387ac44ab333066763f8595
unknown <Abhinavkulkarni997>
2025-12-18 17:14:55 +0530

    new features and files added

---
+++ Backend/models/GuestHouseResponse.js
@@ -0,0 +1,10 @@
+import { response} from "express";
+import mongoose, { Mongoose } from "mongoose";
+
+const GuestHouseResponseSchema=new mongoose.Schema({
+    responses:{type:Object,required:true},
+    submittedAt:{type:Object,default:Date.now}
+})
+
+export default mongoose.model("GuestHouseResponse",GuestHouseResponseSchema);
+
