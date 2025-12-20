43b7ce93eec4e729ad482abb1464a0f8e9ee7698
Abhinav Kulkarni <Abhinavkulkarni64@gmail.com>
2025-12-21 00:46:40 +0530

    new files and features added

--- Frontend/src/components/Success/Success.jsx
+++ Frontend/src/components/Success/Success.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import SuccessImg from '../../assets/Success/success.png';
-import {ueNavigate} from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 const  Success= () => {
   const navigate=useNavigate();
   return (
