fd62c9d60863d7ebceaacacdf0a5381622942018
Abhinav Kulkarni <Abhinavkulkarni64@gmail.com>
2026-01-01 23:54:15 +0530

    new files and features added

--- Frontend/src/Admin/Components/BookingsTab.jsx
+++ Frontend/src/Admin/Components/BookingsTab.jsx
@@ -8,16 +8,24 @@ const BookingsTab=({bookings,onApprove,onReject,onAllocate})=>{
 
     const filteredBookings=bookings.filter(
         (b)=>b.status===activeTab
-    )
+    );
     return(
         <div className='mb-4  border-default'>
             {/* Tabs */}
-            {<ul className="flex  border-b mb-6">
-               {tabMenu.map((tab)=>(
+            {<ul className="flex gap-4  border-b mb-6 overflow-x-auto scrollbar-hide">
+               {tabMenu.map((tab)=>{
+                const count=bookings.filter(b=>b.status===tab).length;
+                return (
                 <li key={tab}
-                 className={`cursor-pointer font-medium px-4 py-2   ${activeTab===tab ?"border-b-2 border-cyan-600 text-cyan-600":"text-gray-500 hover:tet-cyan-600"}`}
-                 onClick={()=>setActiveTab(tab)}>{tab}</li>
-               ))} 
+                 className={`cursor-pointer font-medium px-4 py-2  flex items-center 
+                    gap-2  ${activeTab===tab ?"border-b-2 border-cyan-600 text-cyan-600":"text-gray-500 hover:text-cyan-600"}`}
+                 onClick={()=>setActiveTab(tab)}>{tab}
+                 <span className='text-xs bg-gray-200 px-2  rounded-full'>
+                    {count}
+                 </span>
+                 </li>
+                )
+               })} 
                 
                 </ul> }
 
