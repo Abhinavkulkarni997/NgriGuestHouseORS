f6e7ef51b6293242c3d61ebeefdc24e48fc53183
unknown <Abhinavkulkarni997>
2026-01-01 17:51:18 +0530

    new features and files added

--- Frontend/src/Admin/Components/BookingsTab.jsx
+++ Frontend/src/Admin/Components/BookingsTab.jsx
@@ -1,8 +1,9 @@
 import React,{useState} from 'react';
+import BookingCard from './BookingCard';
 
 const BookingsTab=({bookings,onApprove,onReject,onAllocate})=>{
 
-    const [activeTab, setActiveTab] = useState(0);
+    const [activeTab, setActiveTab] = useState("PENDING");
 
     const tabMenu=[{id:0,name:'PENDING',title:'PENDING'},
         {id:1,name:'APPROVED',title:'APPROVED'},
@@ -13,17 +14,28 @@ const BookingsTab=({bookings,onApprove,onReject,onAllocate})=>{
     const handleTabClick=(id)=>{
         setActiveTab(id);
     }
+
+    const filteredBookings=bookings.filter(
+        (b)=>b.status===activeTab
+    )
     return(
-        <div className=''>
-            {<ul className="flex border-b mb-4">
+        <div className='mb-4 border-b border-default'>
+            {<ul className="flex flex-wrap border-b mb-4">
                {tabMenu.map((tab)=>{
                 <li key={tab.id} className="cursor-pointer px-4 py-2 border-b-2 border-transparent hover:border-blue-500" onClick={()=>handleTabClick(tab.id)}>{tab.title}</li>
                })} 
                 
                 </ul> }
-           
-
-        </div>
+           {
+            filteredBookings.map((booking)=>{
+                <BookingCard
+                key={booking._id}
+                booking={booking}
+                onApprove={onApprove}
+                onReject={onReject}
+                onAllocate={onAllocate}
+            />})}
+           </div>
     )
 }
 
