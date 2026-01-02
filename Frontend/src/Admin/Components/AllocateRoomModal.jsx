36238f53a8379c3e9cddaf03e81e12948877b3a4
unknown <Abhinavkulkarni997>
2026-01-02 11:34:29 +0530

    new features and files added

--- Frontend/src/Admin/Components/AllocateRoomModal.jsx
+++ Frontend/src/Admin/Components/AllocateRoomModal.jsx
@@ -41,25 +41,27 @@ const AllocateRoomModal=({bookingId,onClose,onSuccess})=>{
                 <h2 className='text-xl font-semibold mb-4'>Allocate Room for Booking ID: {bookingId}</h2>
                 <div className='mb-4'>
                     <label className='block text-sm font-medium text-gray-700 mb-2'>Select Room:</label>
-                    <select value={selectedRoom} onChange={(e)=>setSelectedRoom(e.target.value)}>
+                    <select value={selectedRoom} onChange={(e)=>setSelectedRoom(e.target.value)} className='w-full border rounded px-3 py-2'>
+                        <option value="">Select Room</option>
                         {rooms.map((room)=>(
-                            <option key={room.id} value={room.id}>
-                                {room.name}
+                            <option key={room._id} value={room._id}>
+                               Room {room.roomNumber} ({room.roomType})
                             </option>
                         ))}
                     </select>
             </div>
 
-            <div className='flex justify-end mb-4'>
+            <div className='flex justify-end gap-3 pt-4'>
                 <button onClick={onClose} className=''>
                     Cancel
                 </button>
-                {loading?:}
+                <button onClick={handleAllocate} className='px-4 py-2 bg-cyan-600 hover:bg-cyan-700 text-white rounded-lg' disabled={loading}>
+                         {loading?'Allocating...':'Confirm Allocation'}
+                </button>
+           
             </div>
         </div>
         </div>
 )
 }
-
-
 export default AllocateRoomModal;
