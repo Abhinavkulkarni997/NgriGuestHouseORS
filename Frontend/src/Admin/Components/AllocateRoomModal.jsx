7c6c104f7f464627a110992b4f15dbff156010ae
unknown <Abhinavkulkarni997>
2026-01-02 17:04:08 +0530

    new features and files added

--- Frontend/src/Admin/Components/AllocateRoomModal.jsx
+++ Frontend/src/Admin/Components/AllocateRoomModal.jsx
@@ -38,11 +38,11 @@ const AllocateRoomModal=({booking,bookingId,onClose,onSuccess})=>{
     }
 
     return (
-        <div className="fixed inset-0 bg-black/40 bg-opacity-50 flex items-center justify-center z-50">
+        <div className="fixed inset-0  bg-black/40 bg-opacity-50 flex items-center justify-center z-50">
             <div className='bg-white rounded-lg shadow-lg p-6 w-full max-w-md space-y-4'>
                 <h1 className='text-lg font-extrabold mb-4 '>Allocate Room</h1>
-                {/* <h2 className='text-lg font-semibold mb-4'>Applicant Name: {applicantName}</h2>
-                <h2 className='text-lg font-semibold mb-4'>Allocate Room for Booking ID: {bookingId}</h2> */}
+                <h2 className='text-lg font-semibold mb-4'>Applicant Name: {booking.name}</h2>
+                <h2 className='text-lg font-semibold mb-4'>Allocate Room for Booking ID: {bookingId}</h2>
                
                     <label className='block text-sm font-medium text-gray-700 mb-2'>Select Room:</label>
                     <select value={selectedRoom} onChange={(e)=>setSelectedRoom(e.target.value)} className='w-full border rounded px-3 py-2'>
@@ -53,6 +53,12 @@ const AllocateRoomModal=({booking,bookingId,onClose,onSuccess})=>{
                             </option>
                         ))}
                     </select>
+
+                    {
+                        rooms.length===0 &&(
+                            <p className='text-red-600 mt-2'>No rooms available for allocation.</p>
+                        )
+                    }
         
 
             <div className='flex justify-end gap-3 pt-4'>
