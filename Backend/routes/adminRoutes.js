46f408502d35d026e8dec88553d0365395e622c0
unknown <Abhinavkulkarni997>
2026-01-07 11:31:03 +0530

    new features and files added

--- Backend/routes/adminRoutes.js
+++ Backend/routes/adminRoutes.js
@@ -1,15 +1,15 @@
 const express=require('express');
 const router=express.Router();
 const {getAllBookings,approveBooking,rejectBooking, idCardView,allocateRoom,getAvailableRooms,vacateRoom, getCalendarBookings}=require('../controllers/adminBookingcontroller');
-
-router.get('/bookings',getAllBookings);
+const {adminAuth}=require('../middleware/adminAuth');
+router.get('/bookings',adminAuth,getAllBookings);
 // router.patch("/bookings/:id/status",updateBookingStatus);
 
-router.patch('/bookings/:id/approve',approveBooking);
-router.patch('/bookings/:id/reject',rejectBooking);
-router.patch("/bookings/:id/allocate-room",allocateRoom);
-router.patch("/bookings/:id/vacate-room",vacateRoom);
-router.get('/rooms/available',getAvailableRooms);
-router.get('/bookings/:id/idCard',idCardView);
-router.get('/bookings/calendar',getCalendarBookings);
+router.patch('/bookings/:id/approve',adminAuth,approveBooking);
+router.patch('/bookings/:id/reject',adminAuth,rejectBooking);
+router.patch("/bookings/:id/allocate-room",adminAuth,allocateRoom);
+router.patch("/bookings/:id/vacate-room",adminAuth,vacateRoom);
+router.get('/rooms/available',adminAuth,getAvailableRooms);
+router.get('/bookings/:id/idCard',adminAuth,idCardView);
+router.get('/bookings/calendar',adminAuth,getCalendarBookings);
 module.exports=router;
