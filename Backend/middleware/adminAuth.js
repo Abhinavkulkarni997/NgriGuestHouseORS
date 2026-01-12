2b6a5c5fdc43c51799655466a6986d3b3b9092cf
Abhinav Kulkarni <Abhinavkulkarni64@gmail.com>
2026-01-12 23:35:38 +0530

    new files and features added

--- Backend/middleware/adminAuth.js
+++ Backend/middleware/adminAuth.js
@@ -1,14 +1,14 @@
 const jwt=require("jsonwebtoken");
 const adminAuth=(req,res,next)=>{
-    // const token=req.cookies.adminToken;
-    const authHeader=req.headers.authorization;
-    // if(!token){
-    //     return res.status(401).json({message:"Unauthorized"});
-    // }
-       if(!authHeader || !authHeader.startsWith("Bearer")){
+     const token=req.cookies.adminToken || req.headers.authorization?.split(" ")[1];
+    // const authHeader=req.headers.authorization;
+    if(!token){
         return res.status(401).json({message:"Unauthorized"});
     }
-    const token=authHeader.split(" ")[1];
+    //    if(!authHeader || !authHeader.startsWith("Bearer")){
+    //     return res.status(401).json({message:"Unauthorized"});
+    // }
+    // const token=authHeader.split(" ")[1];
     
     try{
         const decoded=jwt.verify(token,process.env.JWT_SECRET);
