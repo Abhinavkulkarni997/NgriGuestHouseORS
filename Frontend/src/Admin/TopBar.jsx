cfaee2a20b0bae20be52b1b63d91384245c3d995
unknown <Abhinavkulkarni997>
2026-01-15 12:45:59 +0530

    new features and files added

--- Frontend/src/Admin/TopBar.jsx
+++ Frontend/src/Admin/TopBar.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
  import sidemenu from '../assets/AdminDashboard/Home/menu.svg';
-const TopBar = ({onToggle}) => {
+const TopBar = ({onToggleDesktop,onToggleMobile}) => {
   return (
  
-        <header className='h-14 flex items-center  px-4 border-b '>
-                  <button onClick={onToggle} className='p-2'>
+        <header className='h-14 flex items-center  px-4 border-b bg-white sticky top-0 z-20'>
+                  <button onClick={onToggleMobile} className='p-2 '>
                     <img src={sidemenu} className="w-6 h-6"/></button>
                 <h1 className='ml-4 font-semibold'>Admin Dashboard</h1>
 
