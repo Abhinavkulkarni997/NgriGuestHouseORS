517c6f67c3b771fa8acc3c3c84a1d1294d83c258
unknown <Abhinavkulkarni997>
2026-01-19 18:12:44 +0530

    new features and files added

--- Backend/routes/adminRoomRoutes.js
+++ Backend/routes/adminRoomRoutes.js
@@ -1,11 +1,12 @@
 const express=require("express");
 const router=express.Router();
 const adminAuth=require('../middleware/adminAuth');
-const {getRoomOccupancy,getRoomHistory,getRoomCalendar}=require('../controllers/roomController');
+const {getRoomOccupancy,getRoomHistory,getRoomCalendar,getRoomDirectory}=require('../controllers/roomController');
 
 router.get("/occupancy",adminAuth,getRoomOccupancy);
 router.get("/:roomId/history",adminAuth,getRoomHistory);
 router.get("/calendar",adminAuth,getRoomCalendar);
+router.get("/",adminAuth,getRoomDirectory);
 
 
 
