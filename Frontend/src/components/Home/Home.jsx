42261f9da94f265462481a5da362ae6d4a016cfa
unknown <Abhinavkulkarni997>
2026-02-05 17:59:24 +0530

    new features and files added

--- Frontend/src/components/Home/Home.jsx
+++ Frontend/src/components/Home/Home.jsx
@@ -1,17 +1,33 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import Hero from '../Hero/Hero';
 import GuestHouseCharges from '../Charges/GuestHouseCharges';
 import Guidelines from '../Guidelines/Guidelines';
 import GuestHouseGallery from '../GuestHouseGallery/GuestHouseGallery';
 // import GuestHouseGallerySwiper from '../GuestHouseGallery/GuestHouseGallerySwiper';
 // import ScrollToTop from '../ScrollToTop/ScrollToTop';
+import {useLocation} from 'react-router-dom';
 
 const Home = () => {
+  const {hash}=useLocation();
+
+  useEffect(()=>{
+    if(hash){
+      
+    }
+
+  },[hash])
+
+
   return (
      <>
      <Hero/>
-      <GuestHouseCharges />
+     <section id="charges" className=''>
+        <GuestHouseCharges />
+     </section>
+  
+    <section id="guidelines" className=''>
       <Guidelines />
+      </section>
       <GuestHouseGallery/>
       {/* <ScrollToTop/> */}
       {/* <GuestHouseGallerySwiper/> */}
