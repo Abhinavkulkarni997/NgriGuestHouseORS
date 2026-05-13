c13e534f4c50631c7448a49fd461b469f013ae8b
unknown <Abhinavkulkarni997>
2026-05-13 12:31:00 +0530

    Application Created date is added

--- Frontend/src/Admin/Components/BookingCard.jsx
+++ Frontend/src/Admin/Components/BookingCard.jsx
@@ -405,9 +405,9 @@ const BookingCard = ({booking,onApprove,onReject,onAllocate,onVacate}) => {
             <b>Organization:</b>{booking.organization}
             </p>
           <p className='text-sm mt-1'>
-             <b>Arrival:</b>{" "}{new Date(booking.arrivalDateTime).toLocaleString()}{" "}<br/>
-             <b>Departure:</b>{" "}{new Date(booking.departureDateTime).toLocaleString()}</p>
-             <b>Application Submitted On:</b>{" "} {new Date(booking.createdAt).toLocaleString()}
+             <b>Arrival:</b>{" "}{new Date(booking.arrivalDateTime).toLocaleString("en-IN")}{" "}<br/>
+             <b>Departure:</b>{" "}{new Date(booking.departureDateTime).toLocaleString("en-IN")}</p>
+             <b>Application Submitted On:</b>{" "} {new Date(booking.createdAt).toLocaleString("en-IN")}
             </div>
        
         {/* Body */}
